86fea00f8f7435f454299a0ff53c4735f9baeafe
davidbogomolov <david.bogomolov2@gmail.com>
2024-03-12 22:36:22 +0200

    feat: added new feature for pre-commiting msg

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,26 @@
-'use strict'
+'use strict';
 
-module.exports = { extends: ['@commitlint/config-conventional'] }
+const expectedTypes = ['feat', 'fix', 'test', 'build', 'refactor', 'docs'];
+
+module.exports = {
+  // extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        'custom-type-enum': ({ type }) => {
+          if (!expectedTypes.includes(type)) {
+            return [false,
+              `
+              Type must be one of: ${expectedTypes.join(', ')} \n
+              Example: feat: add new feature
+              `];
+          }
+          return [true];
+        },
+      },
+    },
+  ],
+  rules: {
+    'custom-type-enum': [2, 'always'],
+  },
+};
