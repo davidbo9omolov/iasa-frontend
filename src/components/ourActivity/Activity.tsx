1df30e2458f2802f4d205ec988c80745be527422
david <david.bogomolov@gmail.com>
2024-05-09 22:10:25 +0300

    feat: translated Activity page

--- src/components/ourActivity/Activity.tsx
+++ src/components/ourActivity/Activity.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { socialNetworks } from '@/constants/constants.ts'
 import { CustomLink } from 'components/customLink/CustomLink.tsx'
@@ -8,6 +9,7 @@ import IphoneCamera from '@/assets/iphoneCamera.png'
 
 const Activity = () => {
   const sectionRef = useRef<HTMLDivElement>(null)
+  const { t } = useTranslation('home')
 
   useEffect(() => {
     const handleScroll = () => {
@@ -66,14 +68,8 @@ const Activity = () => {
     <section ref={sectionRef} className={'min-h-[3000px] flex justify-center  relative  visible'}>
       <div className={'w-[95%] flex justify-between items-center sticky top-[10%] visible h-fit'}>
         <div className={'w-1/3 mt-5'}>
-          <p className={'text-4xl font-bold mb-4'}>
-            Діяльність <br /> Cтудради
-          </p>
-          <p className={'my-6 text-gray'}>
-            Це захист прав та інформування студентів, вирішення адміністративних питань. Наші зусилля націлені на
-            покращення навчання та створення умов, щоб було зручно здобувати знання. Також ми добре розуміємося на
-            створенні якісного відпочинку й проведення часу з користю
-          </p>
+          <p className={'text-4xl font-bold mb-4'}>{t('ourActivity.title')}</p>
+          <p className={'my-6 text-gray'}>{t('ourActivity.description')}</p>
           {socialNetworks.map((network, index) => (
             <Link key={index} to={network.path} className={'underline mr-5'}>
               {network.name}
@@ -90,17 +86,13 @@ const Activity = () => {
           </div>
         </div>
         <div className={'w-1/4 mt-5 relative'}>
-          <p className={'my-3 text-gray  scroll-opacity-text'}>
-            Сайт призначений для інформування студентства щодо діяльності Студради ІПСА, знайомства з нашою командою,
-            полегшення пошуку необхідної інформації для абітурієнтів й привернення уваги до всіх онлайн-ресурсів та
-            вебпродуктів ІПСА.
-          </p>
+          <p className={'my-3 text-gray  scroll-opacity-text'}>{t('ourActivity.animatedDescriptionOne')}</p>
           <p className={'my-3 text-gray absolute scroll-opacity-text top-0'}>
-            Сайт стане в нагоді не тільки ІПСАшникам, а й абітурієнтам, котрі ще думають про вступ до нашого інституту.
+            {t('ourActivity.animatedDescriptionTwo')}
           </p>
-          <p className={'text-2xl font-bold mb-4'}>Ще більше фото тут</p>
+          <p className={'text-2xl font-bold mb-4'}> {t('ourActivity.more')}</p>
           <CustomLink link={'#'} className={'bg-tertiary w-fit rounded-full px-3 py-1 text-primary'}>
-            Дивитися
+            {t('ourActivity.check')}
           </CustomLink>
         </div>
       </div>
