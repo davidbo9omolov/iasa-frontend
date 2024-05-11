bd916fee4db7109f189b32ece7a115b148a08fa7
david <david.bogomolov@gmail.com>
2024-05-11 16:40:22 +0300

    feat: ourTeam page

--- src/components/our-team/OurTeam.tsx
+++ src/components/our-team/OurTeam.tsx
@@ -1,17 +1,22 @@
 import { useTranslation } from 'react-i18next'
 import { teamCardsInformation } from '@/constants/constants.ts'
 import TeamCards from '@/components/team-cards/TeamCards.tsx'
+import { CustomLink } from 'components/customLink/CustomLink.tsx'
 
 const OurTeam = () => {
   const { t } = useTranslation('home')
 
   return (
-    <section className={'min-h-[500px] flex justify-center '}>
+    <section className={'min-h-[250px] flex justify-center mb-48'}>
       <div className={'w-[95%] flex justify-between'}>
-        <div>
+        <div className={'w-[30%]'}>
           <p className={'text-4xl font-bold '}>{t('ourTeam.title')}</p>
+          <p className={'my-6'}>{t('ourTeam.description')}</p>
+          <CustomLink withArrow={false} className={'bg-tertiary w-fit rounded-full p-2'}>
+            <p className={'text-primary'}>{t('ourTeam.more')}</p>
+          </CustomLink>
         </div>
-        <div className={'flex flex-wrap'}>
+        <div className={'flex flex-wrap w-[60%] justify-start'}>
           {teamCardsInformation.map((item, index) => (
             <TeamCards key={index} item={item} />
           ))}
--- src/components/team-cards/TeamCards.tsx
+++ src/components/team-cards/TeamCards.tsx
@@ -10,11 +10,16 @@ type TeamCardProps = {
 
 const TeamCards: React.FC<TeamCardProps> = ({ item }) => {
   return (
-    <div className={'h-fit w-[250px]'}>
-      <p>
-        {item.name} {item.surname}
-      </p>
-      <p>{item.position}</p>
+    <div className={'h-fit w-[350px] bg-quaternary rounded-lg flex p-3 mr-[30px] mb-[30px]'}>
+      <div className={'mr-3'}>
+        <img src={'https://via.placeholder.com/75'} alt={'team'} className={'rounded-full'} />
+      </div>
+      <div>
+        <p>
+          {item.name} {item.surname}
+        </p>
+        <p>{item.position}</p>
+      </div>
     </div>
   )
 }
