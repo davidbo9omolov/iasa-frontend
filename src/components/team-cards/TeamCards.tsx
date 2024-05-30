d22265a38804a7270abc43174641f62ce143beca
Ievgen Bogomolov <ievgen.bogomolov@gmail.com>
2024-05-30 14:26:25 +0300

    feat: repeated animation

--- src/components/team-cards/TeamCards.tsx
+++ src/components/team-cards/TeamCards.tsx
@@ -28,7 +28,6 @@ const TeamCards: React.FC<TeamCardProps> = ({ item, onMoreInfo }) => {
             delay: 0.5,
           },
         }}
-        viewport={{ once: true }}
       >
         <div className={'mr-3'}>
           <img src={'https://via.placeholder.com/75'} alt={'team'} className={'rounded-full'} />
