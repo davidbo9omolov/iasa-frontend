b05499b4d0e037ef5b2696634600d130e4ededa3
David Bogomolov <david.bogomolov2@gmail.com>
2024-09-18 17:17:29 +0300

    fix: fixed issue with video

--- src/components/video-component/VideoComponent.tsx
+++ src/components/video-component/VideoComponent.tsx
@@ -4,7 +4,7 @@ type VideoComponentProps = {
 
 const VideoComponent = ({ src }: VideoComponentProps) => {
   return (
-    <video id="screen" autoPlay muted loop className={`object-cover w-full h-full`} key={src}>
+    <video id="screen" autoPlay muted loop className={`object-cover w-full h-full z-10`} key={src}>
       <source src={src} type={'video/mp4'} />
       Your browser does not support HTML5 video.
     </video>
