b4cafe7f7fc6e458d4a4757fb4d70888217d3b3f
David <david.bogomolov2@gmail.com>
2025-08-17 15:29:30 +0300

    feat: hiding button in case isFullscreenAvailable is false

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+interface ExtendedHTMLElement extends HTMLElement {
+  webkitRequestFullscreen?: () => Promise<void>
+  mozRequestFullScreen?: () => Promise<void>
+  msRequestFullscreen?: () => Promise<void>
+}
+
+export function isFullscreenSupported(): boolean {
+  const element = document.documentElement as ExtendedHTMLElement
+  return !!(
+    element.requestFullscreen ||
+    element.webkitRequestFullscreen ||
+    element.mozRequestFullScreen ||
+    element.msRequestFullscreen
+  )
+}
